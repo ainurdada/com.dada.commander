858c7e3834bbece5647c1d84d593d568f9dce109
Ainur Gazizov <ainurdada@gmail.com>
2023-05-02 03:40:45 +0300

    added events to ConsoleController

--- UI/ConsoleController.cs
+++ UI/ConsoleController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,51 @@ namespace Dada.Commander.UI
 {
     public static class ConsoleController
     {
+        #region ConsoleUI Events
+        public static event Action onShowed
+        {
+            add
+            {
+                if (ConsoleUI.Instance == null)
+                {
+                    Debug.LogError("ConsoleUI is not exist");
+                    return;
+                }
+                ConsoleUI.Instance.OnShowed += value;
+            }
+            remove
+            {
+                if (ConsoleUI.Instance == null)
+                {
+                    Debug.LogError("ConsoleUI is not exist");
+                    return;
+                }
+                ConsoleUI.Instance.OnShowed -= value;
+            }
+        }
+        public static event Action onHided
+        {
+            add
+            {
+                if (ConsoleUI.Instance == null)
+                {
+                    Debug.LogError("ConsoleUI is not exist");
+                    return;
+                }
+                ConsoleUI.Instance.OnHided += value;
+            }
+            remove
+            {
+                if (ConsoleUI.Instance == null)
+                {
+                    Debug.LogError("ConsoleUI is not exist");
+                    return;
+                }
+                ConsoleUI.Instance.OnHided -= value;
+            }
+        }
+        #endregion
+
         public static void ChangeOpenCloseState()
         {
             ConsoleUI.Instance.ChangeOpenCloseState();
